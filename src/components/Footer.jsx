f32ba98a558d6353739d8410488aa1efc36a8a37
abhijit <abhidutta338@gmail.com>
2021-05-17 22:04:55 +0530

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Created by Abhijit Dutta</p>
+      <p>Note Keeper @ {year}</p>
     </footer>
   );
 }
